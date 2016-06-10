8cf7bb6a23b9a07453841ef3ddf6623aa30b405c
ericchan2012 <2570186263@qq.com>
2016-06-10 21:54:10 +0800

    the initial commit

---
+++ cms/search/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.contrib.flatpages.models import FlatPage
+
+def search(request):
+    query = request.GET['q']
+    return render_to_response('search/search.html',
+        { 'query': query,
+        'results': FlatPage.objects.filter(content__icontains=query) })
